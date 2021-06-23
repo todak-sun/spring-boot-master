ae64004f7a1c1ed707a1fd0c6527d17a781a31be
todak-sun <tjsdydwn@gmail.com>
2021-06-23 22:58:10 +0900

    initialize

---
+++ src/main/java/io/todak/study/springbootmaster/SpringBootMasterApplication.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/todak/study/springbootmaster/SpringBootMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
