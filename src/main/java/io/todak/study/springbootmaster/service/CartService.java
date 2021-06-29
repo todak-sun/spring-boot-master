c16ae94978a1d1028e4321fff328c5ba1d2dcdd2
todak-sun <tjsdydwn@gmail.com>
2021-06-30 00:19:03 +0900

    reactor logging & blocking soruce code check with blockhound

--- src/main/java/io/todak/study/springbootmaster/service/CartService.java
+++ src/main/java/io/todak/study/springbootmaster/service/CartService.java
@@ -21,7 +21,9 @@ public class CartService {
 
     public Mono<Cart> addToCart(String cartId, String id) {
         return this.cartRepository.findById(cartId)
+                .log("foundCart")
                 .defaultIfEmpty(new Cart(cartId))
+                .log("emptyCart")
                 .flatMap(cart -> cart.getCartItems().stream()
                         .filter(cartItem -> cartItem.getItem()
                                 .getId().equals(id))
@@ -31,12 +33,19 @@ public class CartService {
                             return Mono.just(cart);
                         })
                         .orElseGet(() -> this.itemRepository.findById(id)
+                                .log("fetchedItem")
                                 .map(CartItem::new)
+                                .log("cartItem")
                                 .map(cartItem -> {
                                     cart.getCartItems().add(cartItem);
                                     return cart;
-                                })))
-                .flatMap(this.cartRepository::save);
+                                })
+                                .log("addedCartItem")
+                        ))
+                .log("cartWithAnotherItem")
+                .flatMap(this.cartRepository::save)
+                .log("savedCart")
+                ;
     }
 
 }
