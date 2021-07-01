a75a1c635ff3130cb8ef22cd2e9c5d1f9561c0e0
todak-sun <tjsdydwn@gmail.com>
2021-07-01 23:45:09 +0900

    module 분리 계속...

---
+++ reactive-3/src/main/java/io/todak/study/springbootmaster/reactive3/Reactive3Application.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster.reactive3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reactive3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reactive3Application.class, args);
+    }
+
+}
---
+++ reactive-3/src/test/java/io/todak/study/springbootmaster/reactive3/Reactive3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster.reactive3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reactive3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
