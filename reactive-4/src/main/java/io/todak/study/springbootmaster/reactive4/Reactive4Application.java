ef0a640653d168c023c8557297488d92e5a7b970
todak-sun <tjsdydwn@gmail.com>
2021-07-02 23:23:33 +0900

    add reactive 4

---
+++ reactive-4/src/main/java/io/todak/study/springbootmaster/reactive4/Reactive4Application.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster.reactive4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reactive4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reactive4Application.class, args);
+    }
+
+}
---
+++ reactive-4/src/test/java/io/todak/study/springbootmaster/reactive4/Reactive4ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster.reactive4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reactive4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
