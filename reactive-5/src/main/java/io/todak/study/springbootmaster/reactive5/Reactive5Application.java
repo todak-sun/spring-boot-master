648957be5628a088e3bd14bcecd4ee4cd93dd9df
todak-sun <tjsdydwn@gmail.com>
2021-07-03 22:38:16 +0900

    add reactive-5

---
+++ reactive-5/src/main/java/io/todak/study/springbootmaster/reactive5/Reactive5Application.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster.reactive5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reactive5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reactive5Application.class, args);
+    }
+
+}
---
+++ reactive-5/src/test/java/io/todak/study/springbootmaster/reactive5/Reactive5ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootmaster.reactive5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reactive5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
