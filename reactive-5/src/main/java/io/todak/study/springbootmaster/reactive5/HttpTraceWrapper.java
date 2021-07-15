52f7be7159bb083ea20c08447bdeaea66cfcca62
todak-sun <tjsdydwn@gmail.com>
2021-07-15 23:54:46 +0900

    스프링 운영 정리

--- reactive-5/src/main/java/io/todak/study/springbootmaster/reactive5/HttpTraceWrapper.java
+++ reactive-5/src/main/java/io/todak/study/springbootmaster/reactive5/HttpTraceWrapper.java
@@ -5,6 +5,11 @@ import org.springframework.data.annotation.Id;
 
 public class HttpTraceWrapper {
 
+    /**
+     * HttpTrace에는 키로 사용할만한 속성이 없어, 몽고디비에 따로 저장할 수는 없다.
+     * 따라서, 본 객체를 활용해 저장한다.
+     */
+
     private @Id
     String id; // <1>
     
--- reactive-5/src/main/java/io/todak/study/springbootmaster/reactive5/HttpTraceWrapperRepository.java
+++ reactive-5/src/main/java/io/todak/study/springbootmaster/reactive5/HttpTraceWrapperRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.repository.Repository;
 
 import java.util.stream.Stream;
 
+/**
+ * HttpTraceWrapperRepository가 논블로킹 패러다임을 사용하지 않기 때문에,
+ * 굳이 리액티브 스트림 방식을 적용하지 않는다.
+ */
 public interface HttpTraceWrapperRepository extends Repository<HttpTraceWrapper, String> {
 
-    Stream<HttpTraceWrapper> findAll(); // <1>
+    Stream<HttpTraceWrapper> findAll();
 
-    void save(HttpTraceWrapper trace); // <2>
+    void save(HttpTraceWrapper trace);
 }
